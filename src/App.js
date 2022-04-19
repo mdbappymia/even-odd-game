bd5ce601096b26693211adba9f9e2fbfb5efb43d
Md Bappy Mia <bmia249@radiantds.com>
2022-04-19 19:33:32 +0600

    win count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const [yourOption, setYourOption] = useState("even");
+  const [result, setResult] = useState("Click Generate for result");
+  const [countWin, setCountWin] = useState(0);
+  const [countLose, setCountLose] = useState(0);
+
+  const handleGenerate = (option) => {
+    let num = parseInt(Math.random() * 10);
+    setNumber(num);
+    if (num === 0) {
+      setResult("You get 0");
+      return;
+    }
+    if (option === "even") {
+      if (num % 2 === 0) {
+        setResult("Win");
+        setCountWin(countWin + 1);
+      } else {
+        setResult("Lose");
+        setCountLose(countLose + 1);
+      }
+    }
+    if (option === "odd") {
+      if (num % 2 !== 0) {
+        setResult("Win");
+        setCountWin(countWin + 1);
+      } else {
+        setResult("Lose");
+        setCountLose(countLose + 1);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>This is app</h1>
+      <h2>Your result: {result}</h2>
+      <div>
+        <span>Win:{countWin}</span> <span>Lose: {countLose}</span>
+      </div>
+      <h3>Number: {number}</h3>
+      <h4>Your option: {yourOption}</h4>
+      <select onChange={(e) => setYourOption(e.target.value)}>
+        <option value="even">Even</option>
+        <option value="odd">Odd</option>
+      </select>
+      <br />
+      <button
+        onClick={() => {
+          handleGenerate(yourOption);
+        }}
+      >
+        Generate
+      </button>
     </div>
   );
-}
+};
 
 export default App;
